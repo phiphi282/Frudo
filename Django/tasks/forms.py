7d875497b07eaf778fd6983cc59457a18746f5e3
Jannik Reichert <38723343+jreicht@users.noreply.github.com>
2018-05-12 17:56:58 +0200

    Added docstring comments in forms.py.

--- Django/tasks/forms.py
+++ Django/tasks/forms.py
@@ -4,8 +4,14 @@ from django.utils import timezone
 
 from .models import Label, Task, Comment
 
-
 class CreateLabelForm(forms.ModelForm):
+    """This form class provides a django interface for creating a new label.
+
+    Attributes:
+        label_text: The name of the label
+        label_description: The description of the label
+        label_color: The color of the label
+    """
     class Meta:
         model = Label
         fields = ['label_text', 'label_description', 'label_color']
@@ -14,12 +20,19 @@ class CreateLabelForm(forms.ModelForm):
     label_description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
     label_color = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 
-
 class ProtocolParseForm(forms.Form):
+    """This form class provides a django interface for parsing a protocol and automatically adding correctly given todos.
+
+    Attributes:
+        protocol_url: The URL where a protocol to be parsed is located
+        protocol_text: The text of a protocol that is pasted by hand
+    """
     protocol_url = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), required=False)
     protocol_text = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), required=False)
 
     def clean(self):
+        """Checks whether at least a protocol_url or protocol_text is given.
+        """
         check_form = super(ProtocolParseForm, self).clean()
 
         if not any(
@@ -32,8 +45,13 @@ class ProtocolParseForm(forms.Form):
             self._errors['protocol_url'] = self.error_class([("You must enter at least the protocol url or text")])
             self._errors['protocol_text'] = self.error_class([("You must enter at least the protocol url or text")])
 
-
 class CreateTaskForm(forms.ModelForm):
+    """This form class provides a django interface for creating a new task.
+
+    Attributes:
+        task_text: The title of the newly created task
+        task_description: The description of the newly created task
+    """
     class Meta:
         model = Task
         fields = ['task_text', 'task_description', 'finished_date', 'assignedTo', 'labels', 'important', 'progress']
@@ -43,6 +61,8 @@ class CreateTaskForm(forms.ModelForm):
     #assignedTo = forms.MultiWidget(widget=forms.CheckboxSelectMultiple(attrs={'class':'js-example-basic-multiple'}))
 
     def clean_finished_date(self):
+        """This function checks that the due date of the newly created task is not before the current date.
+        """
         finished = self.cleaned_data['finished_date']
 
         if finished < timezone.now().date():
@@ -51,6 +71,11 @@ class CreateTaskForm(forms.ModelForm):
         return finished
 
 class CreateCommentForm(forms.ModelForm):
+    """This form class provides a django interface for creating a new comment.
+
+    Attributes:
+        comment_text: the text of the comment (i. e., the comment itself)
+    """
     class Meta:
         model = Comment
         fields = ['comment_text']
