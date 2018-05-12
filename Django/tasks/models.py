c16587bbe919a0c57a6076e0e56b95082605ee3a
Jannik Reichert <38723343+jreicht@users.noreply.github.com>
2018-05-12 17:39:16 +0200

    Added docstring comments in models.py.

--- Django/tasks/models.py
+++ Django/tasks/models.py
@@ -6,6 +6,14 @@ from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 class Label(models.Model):
+    """This class provides a label for differentiating tasks into groups.
+
+    Attributes:
+        label_text: The name of the label
+        label_description: The description of the label
+        label_color: The color of the label
+    """
+
     label_text = models.CharField(max_length=32)
     label_description = models.CharField(max_length=129, blank=True, default='')
     label_color = models.CharField(max_length=10)
@@ -42,6 +50,19 @@ class ProtocolParseForm(forms.Form):
 
 # Create your models here.
 class Task(models.Model):
+    """This class represents tasks.
+
+    Attributes:
+        task_text: The task title
+        task_description: A more detailed description of the task
+        finished_date: The due date of the task
+        creation_date: The date on which the task was created
+        important: A boolean value whether the task is important or normal
+        assignedTo: The list of accounts to which the task is assigned
+        labels: The list of labels for this task
+        progress: An integer value (0..100) to describe the progress of the task in percentage
+    """
+
     task_text = models.CharField(max_length=64)
     task_description = models.CharField(max_length=512)
     finished_date = models.DateField()
@@ -73,6 +94,7 @@ class CreateTaskForm(forms.ModelForm):
         return finished
 
 class Subtask(models.Model):
+    """Currently not used."""
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     subtask_text = models.CharField(max_length=512)
     finished_date = models.DateTimeField()
@@ -80,6 +102,14 @@ class Subtask(models.Model):
     is_finished = models.BooleanField(default=False)
 
 class Comment(models.Model):
+    """This class represents comments given on tasks.
+
+    Attributes:
+        comment_text: The text of the comment
+        comment_user: The user who commented
+        comment_task: The task to which the comment refers
+        comment_date: The date on which the comment was given
+    """
     comment_text = models.CharField(max_length=512)
     comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_task = models.ForeignKey(Task, on_delete=models.CASCADE)
