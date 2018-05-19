b0c793d26840eca1cec3d0ecdcd6472bb6281e6d
Phillip <phillip@freitagsrunde.org>
2018-05-19 13:47:47 +0200

    Removed progress from tests

--- Django/tasks/tests.py
+++ Django/tasks/tests.py
@@ -9,7 +9,7 @@ from .models import Task, Label
 # Create your tests here.
 
 def create_task(task_text, task_description, finished_date, creation_date, is_finished, important, progress):
-    return Task.objects.create(task_text=task_text, task_description=task_description, finished_date=finished_date, creation_date=creation_date, is_finished=is_finished, important=important, progress=progress)
+    return Task.objects.create(task_text=task_text, task_description=task_description, finished_date=finished_date, creation_date=creation_date, is_finished=is_finished, important=important)
 
 def create_label(label_text, label_description, label_color):
     return Label.objects.create(label_text=label_text, label_description=label_description, label_color=label_color)
@@ -27,7 +27,7 @@ class CreateTaskTests(TestCase):
     def test_basictask(self):
         login = self.client.login(username='user1', password='12345')
         time = timezone.now()
-        create_task("test", "testi", time, time, False, False, 12)
+        create_task("test", "testi", time, time, False, False)
         response = self.client.get(reverse('tasks:index'))
         self.assertQuerysetEqual(
             response.context['tasks_list'],
