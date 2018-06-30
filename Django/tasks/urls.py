2e053bc41df72e8c194a9fa0399bd36af57b0477
Phillip <phillip@freitagsrunde.org>
2018-06-30 22:43:14 +0200

    fixed bug

--- Django/tasks/urls.py
+++ Django/tasks/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('closed/', views.IndexView.as_view(), name='closedTasks'),
+    path('closed/', views.ClosedTasksView.as_view(), name='closedTasks'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('create/', views.NewTaskView.as_view(), name='create'),
     path('newlabel/', views.NewLabelView.as_view(), name='newlabel'),
