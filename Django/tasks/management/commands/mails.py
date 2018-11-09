411ced445a1506630503339aa2d3664d48bcb535
Phillip <32705812+phiphi282@users.noreply.github.com>
2018-11-09 21:37:24 +0100

    Update mails.py

--- Django/tasks/management/commands/mails.py
+++ Django/tasks/management/commands/mails.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.mail import EmailMessage
 from django.contrib.auth.models import User
 from django.conf import settings
