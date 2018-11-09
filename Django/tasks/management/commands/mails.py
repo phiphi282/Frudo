d75cdf28eeda061ca53e809a27b742b7b9aef085
Phillip <phillip@freitagsrunde.org>
2018-11-09 22:36:08 +0100

    Only send unfinished mails

--- Django/tasks/management/commands/mails.py
+++ Django/tasks/management/commands/mails.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
             return
         users = User.objects.all()
         for user in users:
-            tasks = Task.objects.filter(assignedTo__in=[user.id]).distinct()
+            tasks = Task.objects.filter(is_finished=False, assignedTo__in=[user.id]).distinct()
             if len(tasks) == 0:
                 continue
             email_content = "Heyho,\n\n du hast noch folgende Todos offen:\n\n"
@@ -23,7 +23,7 @@ class Command(BaseCommand):
             mail = EmailMessage('Unbearbeitete ToDos', email_content, settings.EMAIL_HOST_USER, [user.username + "@" + settings.EMAIL_HOST])
             mail.send()
 
-        unassigned = Task.objects.filter(assignedTo=None)
+        unassigned = Task.objects.filter(is_finished=False, assignedTo=None)
         if len(unassigned) == 0:
                 return
         email_content = "Heyho,\n\n folgende Todos sind noch offen und haben keinen Benutzer assigned:\n\n"
