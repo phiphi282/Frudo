f8339f0197a81db94d3eb1ddc9ff8b12011e20cc
Phillip <phillip@freitagsrunde.org>
2018-11-09 14:18:44 +0100

    Add cronjob to send weekly task reminers via mail

---
+++ Django/tasks/management/commands/mails.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.mail import EmailMessage
+from django.contrib.auth.models import User
+from django.conf import settings
+
+from tasks.models import Task
+
+class Command(BaseCommand):
+    help = "Send mails to all users with unfinished polls"
+
+    def handle(self, *args, **options):
+        if (settings.EMAIL_HOST == "example.com"):
+            return
+        users = User.objects.all()
+        for user in users:
+            tasks = Task.objects.filter(assignedTo__in=[user.id]).distinct()
+            if len(tasks) == 0:
+                continue
+            email_content = "Heyho,\n\n du hast noch folgende Todos offen:\n\n"
+            for task in tasks:
+                email_content += "    " + task.task_text + "\n"
+
+            mail = EmailMessage('Unbearbeitete ToDos', email_content, settings.EMAIL_HOST_USER, [user.username + "@" + settings.EMAIL_HOST])
+            mail.send()
+
+        unassigned = Task.objects.filter(assignedTo=None)
+        if len(unassigned) == 0:
+                return
+        email_content = "Heyho,\n\n folgende Todos sind noch offen und haben keinen Benutzer assigned:\n\n"
+
+        for task in unassigned:
+            print(task.task_text)
+            email_content += "    " + task.task_text + "\n"
+
+        mail = EmailMessage('Unbearbeitete ToDos', email_content, settings.EMAIL_HOST_USER, [settings.EMAIL_GROUP_RECEIVE])
+        mail.send()
--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,10 @@ WORKDIR /var/opt/
 
 COPY Django /var/opt/
 COPY requirements.txt /var/opt/
+COPY cron_mail /etc/cron.d/cron_mail
 
-RUN apt update && apt -y install libsasl2-dev python-dev libldap2-dev libssl-dev && pip3 install -r requirements.txt && ./new_db.sh
+RUN apt update && apt -y install libsasl2-dev python-dev libldap2-dev libssl-dev cron && pip3 install -r requirements.txt && ./new_db.sh && chmod 0644 /etc/cron.d/cron_mail && crontab /etc/cron.d/cron_mail
 
 COPY entrypoint.sh /var/opt/
 
-CMD ["bash", "entrypoint.sh"]
+CMD ["cron", "&&", "bash", "entrypoint.sh"]
